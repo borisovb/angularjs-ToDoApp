104b28074a85d888c8249cda1b2cd402b2f40fe7
AleksandarStaikov <apsreat@gmail.com>
2018-03-03 18:42:38 +0100

    task view details added

--- app/tasks/tasks.js
+++ app/tasks/tasks.js
@@ -9,12 +9,24 @@ angular.module('myApp.tasks', ['ngRoute', 'firebase'])
         controller: 'TasksCtrl'
     });
 
+    $routeProvider.when('/task/:id',{
+        templateUrl: 'tasks/task-detail.html',
+        controller: 'TaskDetailsCtrl'
+    });
+
 }])
 
-.controller('TasksCtrl', function($scope, $firebaseArray, $q){
+.controller('TasksCtrl', function($scope, $firebaseArray){
         
     var ref = firebase.database().ref().child('Tasks');
     $scope.data = $firebaseArray(ref);
+})
+
+.controller('TaskDetailsCtrl', function($scope, $firebaseArray, $routeParams){
+    var id = $routeParams.id;
+    var ref = firebase.database().ref().child('Tasks');
+    var list = $firebaseArray(ref);
+    var rec = list.$loaded().then(function(x){ 
+        $scope.data = x.$getRecord(id);
+    });
 });
-
-
