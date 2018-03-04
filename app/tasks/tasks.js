17510149eaaabf56add34d5dd9730d604be4efb3
AleksandarStaikov <apsreat@gmail.com>
2018-03-04 15:13:54 +0100

    task update and delete added

--- app/tasks/tasks.js
+++ app/tasks/tasks.js
@@ -19,14 +19,40 @@ angular.module('myApp.tasks', ['ngRoute', 'firebase'])
 .controller('TasksCtrl', function($scope, $firebaseArray){
         
     var ref = firebase.database().ref().child('Tasks');
-    $scope.data = $firebaseArray(ref);
+    var list = $firebaseArray(ref)
+    $scope.data = list;
+
+    var refProj = firebase.database().ref().child('Projects');
+    $scope.projects = $firebaseArray(refProj);
+    
+    var refEmp = firebase.database().ref().child('Employees');
+    $scope.employees = $firebaseArray(refEmp);
+
+    $scope.DeleteRecord = function(key){
+        var index = list.$indexFor(key);
+        console.log(index);
+        list.$remove(index);
+
+        //TODO : UPDATE OTHER ENTITIES
+    }
 })
 
-.controller('TaskDetailsCtrl', function($scope, $firebaseArray, $routeParams){
+.controller('TaskDetailsCtrl', function($scope, $firebaseArray, $routeParams, $route){
     var id = $routeParams.id;
     var ref = firebase.database().ref().child('Tasks');
     var list = $firebaseArray(ref);
-    var rec = list.$loaded().then(function(x){ 
+    var rec;
+    list.$loaded().then(function(x){ 
         $scope.data = x.$getRecord(id);
+        rec = $scope.data;
+        
     });
+
+    $scope.UpdateRecord = function(){
+        list.$save(rec).then(function(){
+            $route.reload()
+        });
+
+        //TODO : UPDATE OTHER ENTITIES
+    };
 });
