8e70aa8c045a0b8289620001b5acda71a30487b8
AleksandarStaikov <apsreat@gmail.com>
2018-03-18 23:16:56 +0100

    calendar added

--- app/dashboard/dashboard.js
+++ app/dashboard/dashboard.js
@@ -1,6 +1,6 @@
 'use strict'
 
-angular.module('myApp.dashboard', ['ngRoute', 'myApp.data', 'myApp.weather'])
+angular.module('myApp.dashboard', ['ngRoute', 'myApp.data', 'myApp.weather', 'ui.calendar'])
 
 .config(['$routeProvider', function($routeProvider){
     $routeProvider.when('/', {
@@ -19,7 +19,16 @@ angular.module('myApp.dashboard', ['ngRoute', 'myApp.data', 'myApp.weather'])
             return b['CompletionDate'] - a['CompletionDate'];
         });
         loadedTasks.splice(3);
-        console.log(loadedTasks);
         $scope.tasks = loadedTasks;
-    });
+        return loadedTasks;
+    }).then(function(loadedTasks){
+        var calendarEvents = [];
+        loadedTasks.forEach(task => {
+            calendarEvents.push({title : task.Name, start : new Date(task['CreationDate']), end : new Date(task['CompletionDate'])});
+        });
+
+        $('#calendar').fullCalendar({
+            events: calendarEvents
+        });
+    });    
 });
--- app/directives/weather.js
+++ app/directives/weather.js
@@ -14,22 +14,17 @@ angular.module('myApp.weather', ['ngGeolocation'])
             })
                 .then(function (position) {
                     var apiKey = '6719378f816008e9df61a4ba4321afa2';
-                    console.log(position);
                     var lat = position.coords.latitude;
                     var long = position.coords.longitude;
-                    console.log(lat + ", " + long);
 
                     $http.get('http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long +
                         '&units=metric&type=accurate&appid=' + apiKey)
                         .then(function (response) {
-                            console.log(response);
-
                             $scope.temperature = Math.round(response.data.main.temp);
                             $scope.weatherIcon = 'http://openweathermap.org/img/w/' + response.data.weather[0].icon + '.png';
                             $scope.location = response.data.name + ", " + response.data.sys.country;
                             $scope.description = response.data.weather[0].main;
                         }, function (error) {
-                            console.log(error);
                         })
                 })
 
